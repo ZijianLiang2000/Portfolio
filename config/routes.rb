e2d10488454c70ece26b26ba8db0a92b43bbe1fc
Brian Liang <brianbrain2010@hotmail.com>
2022-05-30 22:48:18 +0100

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :personal_portfolios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'personal_portfolios#home'
-
+  root "personal_portfolios#home"
+  get "projects" => "personal_portfolios#projects"
+  get "resume" => "personal_portfolios#resume"
 end
